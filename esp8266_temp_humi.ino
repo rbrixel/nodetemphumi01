af71863eb461d33a54959baa56fe9d824ca9e5f7
R. Brixel <rbrixel@gmail.com>
2020-07-05 13:55:00 +0200

    Added GPS- and BME280-Sensor

--- esp8266_temp_humi.ino
+++ esp8266_temp_humi.ino
@@ -3,32 +3,34 @@
  * DE: ESP-Temperatur- und Luftfeuchtigkeits-Modul
  * 
  * Hardware: 
- * - ESP-12F (ESP8266, "D1 Mini")
- * - DHT22 (in future: BME280 - temperature, humidity and air pressure)
+ * - ESP-12F (ESP8266, "Wemos D1 Mini"-Clone)
+ * - BME280 (temperature, humidity and air pressure)
  * - DS18B20 (waterproof for outdoor measurement)
+ * - ublox Neo-6M GPS-module
  * 
  * Software:
  * - wifi (integrated in esp8266)
- * - MQTT (PubSubClient)
+ * - EspMQTTClient (aka PubSubClient) 1.8.0
  * - Over-The-Air-Update (ArduinoOTA)
- * - OneWire (OneWire)
+ * - OneWire (OneWire - use 2.3.0 for GPIO16/D0!)
+ * - TinyGPSPlus-1.0.2b (http://arduiniana.org/libraries/tinygpsplus/)
+ * - Cactus.io BME280 (http://cactus.io/projects/weather/arduino-weather-station-bme280-sensor)
  * 
  * Setup:
  * - Change SSID of your wifi (STASSID)
  * - Change wifi-password (STAPSK)
  * - Change hostname of your module (host)
  * - Change ip-address of your MQTT-Broker (MQTT_BROKER)
- * - Change DHT-xx-Pin (DHTPIN)
- * - Change DHT-Type: 11 or 22 (DHTTYPE)
  * - Change DS18B20-Pin (ONE_WIRE_BUS)
+ * - Change Software-Serial-Pins (RXPin, TXPin)
  * 
  * Pinmapping Wemos D1 Mini:
- * Board  Arduino/GPIO  Special
- * D0     16        
- * D1     5             SCL - i2c
- * D2     4             SDA - i2c
- * D3     0
- * D4     2
+ * Board  Arduino/GPIO  Special     In use
+ * D0     16                        DS18B20 onewire
+ * D1     5             SCL - i2c   SCL - i2c
+ * D2     4             SDA - i2c   SDA - i2c
+ * D3     0                         RX - Neo6m GPS
+ * D4     2                         TX - Neo6m GPS
  * D5     14            SCK - SPI
  * D6     12            MISO - SPI
  * D7     13            MOSI - SPI
@@ -37,22 +39,22 @@
  * RX     3
  * 
  * Author: René Brixel <mail@campingtech.de>
- * Date: 2020-07-03
+ * Date: 2020-07-02
  */
 
 // WIFI
 #include <ESP8266WiFi.h> // ESP8266
 #ifndef STASSID
-#define STASSID "******" // your wifi-name
-#define STAPSK  "******" // your wifi-password
+#define STASSID "*****" // your wifi-name
+#define STAPSK  "*****" // your wifi-password
 #endif
-const char* host = "******"; // hostname of module
+const char* host = "nodetemphumi01"; // hostname of module
 const char* ssid     = STASSID;
 const char* password = STAPSK;
 
 // MQTT-Client
 #include <PubSubClient.h>
-const char* MQTT_BROKER = "***.***.***.***"; // ip-address of your mqtt-broker
+const char* MQTT_BROKER = "192.168.111.199"; // ip-address of your mqtt-broker
 WiFiClient espClient;
 PubSubClient client(espClient);
 //long lastMsg = 0;
@@ -64,21 +66,31 @@ char msg[50];
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-// DHT
-#include <DHT.h>
-#define DHTPIN 2
-#define DHTTYPE DHT11 // DHT11, DHT22
-DHT dht(DHTPIN, DHTTYPE);
+// BME280
+// SCL - D1 - GPIO5
+// SDA - D2 - GPIO4
+#include <cactus_io_BME280_I2C.h>
+BME280_I2C bme(0x76); // uint i2c-address
 
 // DS18B20
-/*
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#define ONE_WIRE_BUS 3  //Sensor DS18B20
+#define ONE_WIRE_BUS 16 // GPIO16 - D0
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 int sensorCount;
-*/
+
+// GPS
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+static const int RXPin = 0, TXPin = 2; // RXPin = D3, TXPin = D4;
+static const uint32_t GPSBaud = 9600; // Standard 9600, alternative 4800
+float gpsLat = 0.0; // Latitude
+float gpsLon = 0.0; // Longitude
+float gpsSat = 0.0; // Satellites
+float gpsAlt = 0.0; // Altitude
+TinyGPSPlus gps;
+SoftwareSerial ss(RXPin, TXPin);
 
 // Interval-values (for non blocking code)
 unsigned long nbcPreviousMillis = 0; // holds last timestamp
@@ -88,14 +100,16 @@ void setup() {
   // Start serial-connection
   Serial.begin(115200);
 
-  // Start DHT-sensor
-  dht.begin();
+  // Start BME280
+  if (!bme.begin()) {
+    Serial.println("Could not find BME280 sensor, check wiring");
+    while (1);
+  }
+  bme.setTempCal(-1);// Temp was reading high so subtract 1 degree
 
   // Start DS18B20-sensor
-  /*
   sensors.begin();
   sensorCount = sensors.getDS18Count();
-  */
 
   // Start wifi and connect to access point
   setup_wifi();
@@ -106,6 +120,9 @@ void setup() {
   // Start MQTT
   client.setServer(MQTT_BROKER, 1883); // ip-address, port 1883
   //client.setCallback(callback); // only needed for subscription of topic
+
+  // Start Software-serial for gps
+  ss.begin(GPSBaud); // connect gps sensor (standard 9600)
 }
 
 void setup_wifi() {
@@ -241,38 +258,93 @@ void loop() {
   }
   client.loop();
 
+  while (ss.available() > 0) {
+    if (gps.encode(ss.read())) {
+      if (gps.location.isValid()) {
+        gpsLat = gps.location.lat();
+        gpsLon = gps.location.lng();
+      }
+      if (gps.satellites.isValid()) {
+        gpsSat = gps.satellites.value();
+      }
+      if (gps.altitude.isValid()) {
+        gpsAlt = gps.altitude.meters();
+      }
+    }
+  }
+
   // --- non-blocking-code ---
   unsigned long nbcCurrentMillis = millis(); // get actual timestamp
   if (nbcCurrentMillis - nbcPreviousMillis >= nbcInterval) {
     nbcPreviousMillis = nbcCurrentMillis;
 
     Serial.println("- - - - -");
-    Serial.println("Timestamp: " + String(millis()));
+    Serial.println("Uptime (ms): " + String(millis()));
 
-    /*
-    // Check, if ds18b20 is reachable
+    // DS18B20
     if (sensorCount == 0) {
       Serial.println("ds18b20 not found.");
       client.publish("/climate/outdoor/temperature", "error");
     } else {
+      sensors.requestTemperatures(); // Send the command to get temperature readings 
       float outTemp = sensors.getTempCByIndex(0); // read first sensor for outdoor temperature
       
       snprintf (msg, 50, "%.2f", outTemp);
       client.publish("/climate/outdoor/temperature", msg);
-      Serial.println("/climate/outdoor/temperature: " + String(outTemp));
+      Serial.println("/climate/outdoor/temperature: " + String(outTemp) + " (Celsius)");
     }
-    */
 
-    float inHumi = dht.readHumidity(); // read indoor humidity
-    float inTemp = dht.readTemperature(); // read indoor temperature
+    // BME280
+    bme.readSensor();
+    float bTemp = bme.getTemperature_C(); // Temperature in Celsius
+    float bHumi = bme.getHumidity(); // Humidity in Precent
+    float bHp = bme.getPressure_HP(); // pressure in hectapascals
+    float bMb = bme.getPressure_MB(); // pressure in millibars
 
-    snprintf (msg, 50, "%.2f", inTemp);
+    snprintf (msg, 50, "%.2f", bTemp);
     client.publish("/climate/indoor/floor/temperature", msg);
-    snprintf (msg, 50, "%.2f", inHumi);
+    snprintf (msg, 50, "%.2f", bHumi);
     client.publish("/climate/indoor/floor/humidity", msg);
+    snprintf (msg, 50, "%.2f", bHp);
+    client.publish("/climate/indoor/floor/pressure-hp", msg);
+    snprintf (msg, 50, "%.2f", bMb);
+    client.publish("/climate/indoor/floor/pressure-mb", msg);
 
-    Serial.println("/climate/indoor/floor/temperature: " + String(inTemp));
-    Serial.println("/climate/indoor/floor/humidity: " + String(inHumi));
+    Serial.println("/climate/indoor/floor/temperature: " + String(bTemp) + " (Celsius)");
+    Serial.println("/climate/indoor/floor/humidity: " + String(bHumi) + " (%)");
+    Serial.println("/climate/indoor/floor/pressure-hp: " + String(bHp) + " (hectapascals)");
+    Serial.println("/climate/indoor/floor/pressure-mb: " + String(bMb) + " (millibars)");
+
+    // GPS
+    if (gpsLat == 0.0) {
+      client.publish("/position/latitude", "error");
+    } else {
+      snprintf (msg, 50, "%.2f", gpsLat);
+      client.publish("/position/latitude", msg);
+    }
+    if (gpsLon == 0.0) {
+      client.publish("/position/latitude", "error");
+    } else {
+      snprintf (msg, 50, "%.2f", gpsLon);
+      client.publish("/position/longitude", msg);
+    }
+    if (gpsSat == 0.0) {
+      client.publish("/position/latitude", "error");
+    } else {
+      snprintf (msg, 50, "%.0f", gpsSat);
+      client.publish("/position/satellites", msg);
+    }
+    if (gpsAlt == 0.0) {
+      client.publish("/position/latitude", "error");
+    } else {
+      snprintf (msg, 50, "%.2f", gpsAlt);
+      client.publish("/position/altitude", msg);
+    }
+    
+    Serial.println("/position/latitude: " + String(gpsLat) + " (N, Breitengrad)");
+    Serial.println("/position/longitude: " + String(gpsLon) + " (E, Längengrad)");
+    Serial.println("/position/satellites: " + String(gpsSat) + " (number)");
+    Serial.println("/position/altitude: " + String(gpsAlt) + " (meters)");
   }
 }
 
